6a0387d704fe90c1c55f3542a062572d4e6e509d
mahfujur-rh <mahfujurrahman1319@gmail.com>
2025-10-18 23:25:04 +0600

    Create problem-C.cpp

---
+++ problem-C.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        int round=(x+5)/10*10;
+        cout<<100-round<<endl;
+       
+    }
+    return 0;
+}
+
